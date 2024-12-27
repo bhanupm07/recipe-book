de0bb7bcd7cfc0d61e011cf1f80f0cb8352ee512
bhanu <bhanu2003@gmail.com>
2024-12-28 00:23:31 +0530

    modify: changed apiKey

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,3 +1,3 @@
-export const apiKey = "4e1b76e7fe5145b5934089f690514850";
+export const apiKey = "39b5bc90fcc149c8aea90f49c24f4503";
 
 export const serverUrl = "https://api.spoonacular.com/recipes";
